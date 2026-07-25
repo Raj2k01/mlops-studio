682363274d30a6a952c9bb9707ae18023e254612
Rajasekar S <rajasekarsrinivasan95448@gmail.com>
2026-07-25 10:34:05 +0530

    Added Ml config

---
+++ ml/scripts/config.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+RAW_DATA = PROJECT_ROOT / "data" / "raw" / "redwinequality.csv"
+PROCESSED_DATA = PROJECT_ROOT / "data" / "processed"
+
+#models
+MODEL_DIR = PROJECT_ROOT / "models"
+MODEL_FILE = MODEL_DIR / "xgboost_model.pkl"
+
+#outputs
+OUTPUT_DIR = PROJECT_ROOT / "outputs"
+
+# Random Seed
+RANDOM_STATE = 42
+
+# Target Column
+TARGET_COLUMN = "quality"
---
+++ ml/scripts/testconfig.py
@@ -0,0 +1,5 @@
+from config import *
+
+print(RAW_DATA)
+print(MODEL_FILE)
+print(OUTPUT_DIR)
