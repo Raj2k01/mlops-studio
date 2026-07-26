9041233c4c989039fd11a53e59e0a4532f6f076d
Rajasekar S <rajasekarsrinivasan95448@gmail.com>
2026-07-26 07:12:46 +0530

    bridged the gap between springboot and ML using PythonPredictionClient class

--- backend/src/main/java/com/rs/backend/service/PythonPredictionClient.java
+++ backend/src/main/java/com/rs/backend/service/PythonPredictionClient.java
@@ -1,13 +1,63 @@
 package com.rs.backend.service;
+
 import com.rs.backend.dto.PredictionRequest;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 public class PythonPredictionClient implements PredictionClient {
 
+    private static final String INPUT_FILE =
+            "../ml/data/prediction_input.json";
+
+    private static final String OUTPUT_FILE =
+            "../ml/data/prediction_output.json";
+
+    private static final String PYTHON_SCRIPT =
+            "../ml/scripts/predict.py";
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @Override
     public int predict(PredictionRequest request) {
-        return 5;
-    }
 
+        try {
+
+            // Write request to JSON
+            mapper.writeValue(new File(INPUT_FILE), request);
+
+            // Execute Python
+            ProcessBuilder pb = new ProcessBuilder(
+                    "C:\\Users\\Raj\\AppData\\Local\\Programs\\Python\\Python313\\python.exe",
+                    PYTHON_SCRIPT
+            );
+
+            pb.redirectErrorStream(true);
+
+            Process process = pb.start();
+
+            String output = new String(process.getInputStream().readAllBytes());
+
+            int exitCode = process.waitFor();
+
+            System.out.println("Python Output:");
+            System.out.println(output);
+
+            if (exitCode != 0) {
+                throw new RuntimeException(output);
+            }
+
+            // Read prediction_output.json
+            JsonNode node = mapper.readTree(new File(OUTPUT_FILE));
+
+            return node.get("prediction").asInt();
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
